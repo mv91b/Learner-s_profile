2e625d19cee84367c22019ae812717ecea18bf6e
rishikmishra7 <rishikmishratech@gmail.com>
2025-01-15 12:46:50 +0530

    this is Rk

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <h1> Welcome !!! </h1>
         <h2>
-          This is my first change "Rishik Mishra".
+          This is my first change.
         </h2>
 
   
